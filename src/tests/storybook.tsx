6b4b85d1f24b0a1b9a65386a6b6d0ff5c5611e61
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-11-26 09:06:56 +0100

    Add missing compliance

--- src/tests/storybook.tsx
+++ src/tests/storybook.tsx
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 /**
  * Checks if view mode of Storybook is "Docs" overview or Story directly
  * */
