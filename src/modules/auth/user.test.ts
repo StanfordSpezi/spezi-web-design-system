ee519c97f4b2d4d90a2dc112d3741f9e55c69fa8
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-11-26 14:42:20 +0100

    Fix compliance

--- src/modules/auth/user.test.ts
+++ src/modules/auth/user.test.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { getUserName } from '@/modules/auth/user'
 
 describe('user', () => {
