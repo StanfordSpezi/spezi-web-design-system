1743e060ce640e11da157ea26c92a6fe8bd37f74
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-01-02 18:51:37 +0100

    Fix tests

--- src/forms/FormError/FormError.test.tsx
+++ src/forms/FormError/FormError.test.tsx
@@ -9,20 +9,27 @@
 import { render, screen } from '@testing-library/react'
 import { FormError } from '.'
 
+const formError = { message: 'User already exists' }
+
 describe('ErrorState', () => {
-  it('renders error state', () => {
-    render(<FormError>Lorem</FormError>)
+  it('renders error message', () => {
+    render(<FormError formError={formError} />)
 
-    const element = screen.getByRole('alert', { name: 'Lorem' })
-
+    const element = screen.getByText(formError.message)
     expect(element).toBeInTheDocument()
   })
 
-  it('renders entityName', () => {
-    render(<FormError entityName="users" />)
+  it('renders prefix', () => {
+    render(<FormError formError={formError} prefix="Creating user failed. " />)
 
-    const element = screen.getByText(/Fetching\susers\sfailed/)
+    const element = screen.getByText(/Creating user failed/)
+    expect(element).toBeInTheDocument()
+  })
 
+  it('renders completely custom node', () => {
+    render(<FormError formError={<>Lorem</>} />)
+
+    const element = screen.getByRole('alert', { name: 'Lorem' })
     expect(element).toBeInTheDocument()
   })
 })
