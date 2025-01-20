0efe4ad9765272b212ade5734451eef91cd387f5
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-01-20 09:45:39 +0100

    Add Checkbox component (#36)
    # Add Checkbox component
    
    ## :recycle: Current situation & Problem
    Closes #15
    
    
    ## :gear: Release Notes 
    * Add Checkbox component
    
    
    ![image](https://github.com/user-attachments/assets/7dbdc93c-c88a-4987-af75-397b19e6d334)
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,52 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import { useArgs } from '@storybook/core/preview-api'
+import { type Meta, type StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { type ComponentProps } from 'react'
+import { SideLabel } from '@/components/SideLabel'
+import { Checkbox } from './Checkbox'
+
+const meta: Meta<typeof Checkbox> = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: {
+    checked: false,
+    onCheckedChange: fn(),
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Checkbox>
+
+export const Default: Story = {
+  render: function Render(args) {
+    const [, updateArgs] = useArgs<ComponentProps<typeof Checkbox>>()
+    return (
+      <Checkbox
+        {...args}
+        onCheckedChange={(checked) => updateArgs({ checked })}
+      />
+    )
+  },
+}
+
+export const Checked: Story = {
+  ...Default,
+  args: {
+    checked: true,
+  },
+}
+
+export const Labeled = () => (
+  <SideLabel label="Show unread only">
+    <Checkbox />
+  </SideLabel>
+)
---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,35 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import {
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  forwardRef,
+} from 'react'
+import { cn } from '@/utils/className'
+
+type CheckboxProps = ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+
+export const Checkbox = forwardRef<
+  ElementRef<typeof CheckboxPrimitive.Root>,
+  CheckboxProps
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      'focus-ring flex-center peer size-4 shrink-0 rounded border border-input disabled:cursor-not-allowed disabled:opacity-50',
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className="flex-center size-2.5 rounded-sm bg-primary" />
+  </CheckboxPrimitive.Root>
+))
+
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
