b1abf772e82d30e499c2ef57d64a9ce177d5ac10
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-01-20 16:44:15 +0100

    Replace ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+import webConfigs from '@stanfordbdhg/spezi-web-configurations'
+const { eslintConfig } = webConfigs
+
+export default eslintConfig
