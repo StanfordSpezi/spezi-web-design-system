202783fe612e7d2d62097e68d8549c96d69f29b5
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-01-30 20:19:25 +0100

    Bump packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,30 +6,22 @@
 // SPDX-License-Identifier: MIT
 //
 
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
-  },
-  core: {
-    builder: {
-      name: "@storybook/builder-vite",
-      options: {
-        viteConfigPath: "./vite.config.ts",
-      },
-    },
+    options: {},
   },
   docs: {},
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
 };
-
 export default config;
