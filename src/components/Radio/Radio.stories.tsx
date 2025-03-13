ce666541134e124a055647e46fb5d9a9ab895808
Audrey Lin <84480051+ivyalin2015@users.noreply.github.com>
2025-03-13 11:26:44 -0700

    Adding Radio Input component (#27)
    # Adding Radio Input component
    
    ## :recycle: Current situation & Problem
    Closes #16 
    
    
    
    ## :gear: Release Notes 
    - Allows users to select one option from a dropdown list with as many
    items as desired
    - Uses standardized coloring
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).
    
    ---------
    
    Co-authored-by: Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>

---
+++ src/components/Radio/Radio.stories.tsx
@@ -0,0 +1,28 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import { type Meta } from "@storybook/react";
+import { RadioGroup, Radio } from "./Radio";
+import { SideLabel } from "../SideLabel";
+
+const meta: Meta = {
+  title: "Components/Radio",
+};
+
+export default meta;
+
+export const Default = () => (
+  <RadioGroup defaultValue="default" aria-label="View density">
+    <SideLabel label="Default">
+      <Radio value="1" id="r1"></Radio>
+    </SideLabel>
+    <SideLabel label="Second Choice">
+      <Radio value="2" id="r2"></Radio>
+    </SideLabel>
+  </RadioGroup>
+);
---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,37 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import * as RadioPrimitive from "@radix-ui/react-radio-group";
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+import { cn } from "../../utils/className";
+
+/**
+ * Container for Radio inputs
+ * */
+export const RadioGroup = RadioPrimitive.Root;
+
+export const Radio = forwardRef<
+  ElementRef<typeof RadioPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof RadioPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <RadioPrimitive.Item
+    ref={ref}
+    className={cn(
+      "focus-ring flex-center size-4 shrink-0 rounded-full border border-input disabled:cursor-not-allowed disabled:opacity-50",
+      className,
+    )}
+    {...props}
+  >
+    <RadioPrimitive.Indicator className="flex-center size-2 rounded-full bg-primary" />
+  </RadioPrimitive.Item>
+));
+Radio.displayName = RadioPrimitive.Item.displayName;
