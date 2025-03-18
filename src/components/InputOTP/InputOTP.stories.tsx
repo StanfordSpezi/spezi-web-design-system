7c5a829ad0b3ed92125b374dd75dbb2644da0971
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-03-18 15:45:10 +0100

    Remove input

--- src/components/InputOTP/InputOTP.stories.tsx
+++ src/components/InputOTP/InputOTP.stories.tsx
@@ -7,7 +7,6 @@
 //
 
 import { type Meta, type StoryObj } from "@storybook/react";
-import { Input } from "@/components/Input";
 import {
   InputOTP,
   InputOTPGroup,
@@ -28,18 +27,15 @@ type Story = StoryObj<typeof InputOTP>;
 export const Default: Story = { args: { maxLength: 6 } };
 
 export const Custom = () => (
-  <div className="flex gap-2">
-    <Input />
-    <InputOTPRoot maxLength={4}>
-      <InputOTPGroup>
-        <InputOTPSlot index={0} />
-        <InputOTPSlot index={1} />
-      </InputOTPGroup>
-      <InputOTPSeparator />
-      <InputOTPGroup>
-        <InputOTPSlot index={2} />
-        <InputOTPSlot index={3} />
-      </InputOTPGroup>
-    </InputOTPRoot>
-  </div>
+  <InputOTPRoot maxLength={4}>
+    <InputOTPGroup>
+      <InputOTPSlot index={0} />
+      <InputOTPSlot index={1} />
+    </InputOTPGroup>
+    <InputOTPSeparator />
+    <InputOTPGroup>
+      <InputOTPSlot index={2} />
+      <InputOTPSlot index={3} />
+    </InputOTPGroup>
+  </InputOTPRoot>
 );
