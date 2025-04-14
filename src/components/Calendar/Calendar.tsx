01dc6e56d5184149525bf094d6346607e024bdb8
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-04-14 19:27:17 +0200

    Fix compliance

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { isDate, set } from "date-fns";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import {
