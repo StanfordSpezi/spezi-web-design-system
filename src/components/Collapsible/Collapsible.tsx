7a0c2e91e69e5dd22d4d213ec388d62202872f80
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-05-26 22:32:39 +0200

    Prepush

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -34,9 +34,9 @@ import { type AsChildProp } from "@/utils/misc";
  *   </CollapsibleContent>
  * </CollapsibleRoot>
  */
-export const CollapsibleRoot = (props: ComponentProps<typeof CollapsiblePrimitive.Root>) => (
-  <CollapsiblePrimitive.Root data-slot="collapsible-root" {...props} />
-);
+export const CollapsibleRoot = (
+  props: ComponentProps<typeof CollapsiblePrimitive.Root>,
+) => <CollapsiblePrimitive.Root data-slot="collapsible-root" {...props} />;
 
 /**
  * Button that toggles the collapsible.
