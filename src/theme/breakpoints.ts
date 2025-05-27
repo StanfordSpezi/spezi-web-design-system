ffd4ecd770ac1ebca06ee1b83490d15ed0e7c237
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-05-27 09:04:49 +0200

    Add useMedia utility hooks (#76)
    # Add useMedia utility hooks
    
    ## :recycle: Current situation & Problem
    Closes #73 
    
    
    ## :gear: Release Notes
    * Add useMedia utility hooks.
    * `useMedia` for checking any media query
    * `useIsTouchDevice` for checking whether pointer device
    * `useIsScreen` for checking Tailwind compatible breakpoints
    
    
    ## :white_check_mark: Testing
    
    
    https://github.com/user-attachments/assets/25b54c9e-9b44-4fe3-a221-976f17a8f2f3
    
    
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,21 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+/**
+ * Default Tailwind's screen sizes.
+ *
+ * @remarks
+ * It's valid unless overridden.
+ */
+export const screens = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+} as const;
