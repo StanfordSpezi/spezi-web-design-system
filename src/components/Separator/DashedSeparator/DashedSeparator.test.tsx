0caac37ae1df92272cae7c536b9f01345754a059
michaelheckmann <heckmannmichael@yahoo.de>
2025-07-21 12:31:12 +0200

    add DashedSeparator component with customizable styles and tests

---
+++ src/components/Separator/DashedSeparator/DashedSeparator.test.tsx
@@ -0,0 +1,70 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import { render, screen } from "@testing-library/react";
+import { SeparatorText } from "../SeparatorText";
+import { DashedSeparator } from ".";
+
+describe("DashedSeparator", () => {
+  it("renders separator element if no children provided", () => {
+    render(<DashedSeparator />);
+
+    const element = screen.getByRole("separator");
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders regular element if children provided", () => {
+    render(<DashedSeparator>lorem</DashedSeparator>);
+
+    const element = screen.queryByRole("separator");
+    expect(element).not.toBeInTheDocument();
+  });
+
+  it("renders children", () => {
+    render(
+      <DashedSeparator>
+        <SeparatorText>Lorem</SeparatorText>
+      </DashedSeparator>,
+    );
+
+    const element = screen.getByText("Lorem");
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders separator with custom style props", () => {
+    render(
+      <DashedSeparator
+        dashColor="var(--color-primary)"
+        dashSize="8px"
+        dashSpacing="10%"
+      />,
+    );
+
+    const element = screen.getByRole("separator");
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveStyle({
+      "--dash-color": "var(--color-primary)",
+      "--dash-size": "8px",
+      "--dash-spacing": "10%",
+    });
+  });
+
+  it("renders separator with custom style classes", () => {
+    render(
+      <DashedSeparator className="[--dash-color:theme(colors.red-500)] [--dash-size:theme(spacing.4)] [--dash-spacing:theme(spacing.3)]" />,
+    );
+
+    const element = screen.getByRole("separator");
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass(
+      "[--dash-color:theme(colors.red-500)]",
+      "[--dash-size:theme(spacing.4)]",
+      "[--dash-spacing:theme(spacing.3)]",
+    );
+  });
+});
