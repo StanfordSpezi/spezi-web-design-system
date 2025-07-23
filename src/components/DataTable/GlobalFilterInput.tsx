8a050183e405382e51907a8f334a4c9242ac23b6
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-07-23 09:56:26 +0200

    Expose more internals from DataTable (#89)
    # Expose more utilities from DataTable
    
    ## :recycle: Current situation & Problem
    Sometimes it's necessary to opt-out from basic `DataTable` component due
    to more complex design requirements than just modifying the view. In
    such scenarios, `DataTable` should export as much of it's internals to
    handle those opt-out situations with consistency and simplify them.
    
    
    ## :gear: Release Notes
    * Expose GlobalFilterInput and DataTablePagination
    * Split GlobalFilterInput into smaller components
    * Move state computations to useDataTable
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- src/components/DataTable/GlobalFilterInput.tsx
+++ src/components/DataTable/GlobalFilterInput.tsx
@@ -7,30 +7,59 @@
 //
 
 import { Search } from "lucide-react";
+import { type ComponentProps } from "react";
+import { cn } from "@/utils/className";
 import { Input, type InputProps } from "../Input";
 
-interface GlobalFilterInputProps extends InputProps {
+export const GlobalFilterInputContainer = ({
+  className,
+  ...props
+}: ComponentProps<"div">) => (
+  <div className={cn("relative max-w-72 grow", className)} {...props} />
+);
+
+export const GlobalFilterInputIcon = ({
+  className,
+  ...props
+}: ComponentProps<typeof Search>) => (
+  <Search
+    className={cn(
+      "text-muted-foreground absolute top-2.5 left-3 size-5",
+      className,
+    )}
+    aria-hidden="true"
+    {...props}
+  />
+);
+
+interface GlobalFilterInputInputProps extends InputProps {
   entityName?: string;
 }
 
+export const GlobalFilterInputInput = ({
+  entityName,
+  className,
+  ...props
+}: GlobalFilterInputInputProps) => {
+  const placeholder = `Search${entityName ? ` ${entityName}` : ""}...`;
+  return (
+    <Input
+      placeholder={placeholder}
+      aria-label={placeholder}
+      defaultValue=""
+      className={cn("pl-10", className)}
+      {...props}
+    />
+  );
+};
+
+interface GlobalFilterInputProps extends GlobalFilterInputInputProps {}
 /**
  * Renders input for global text search.
  */
-export const GlobalFilterInput = ({
-  entityName,
-  ...props
-}: GlobalFilterInputProps) => {
-  const placeholder = `Search${entityName ? ` ${entityName}` : ""}...`;
-  return (
-    <div className="relative max-w-72 grow">
-      <Search className="text-muted-foreground absolute top-2.5 left-3 size-5" />
-      <Input
-        placeholder={placeholder}
-        aria-label={placeholder}
-        defaultValue=""
-        className="pl-10"
-        {...props}
-      />
-    </div>
-  );
-};
+export const GlobalFilterInput = (props: GlobalFilterInputProps) => (
+  <GlobalFilterInputContainer>
+    <GlobalFilterInputIcon />
+    <GlobalFilterInputInput {...props} />
+  </GlobalFilterInputContainer>
+);
