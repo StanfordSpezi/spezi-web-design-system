a90eb276a70d33c07c2405b766af00ee45e9efba
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-11-10 16:41:37 +0100

    Fix tests

--- testSetup.ts
+++ testSetup.ts
@@ -8,11 +8,11 @@
 
 import "@testing-library/jest-dom";
 
-const ResizeObserverMock = vi.fn(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn(),
-}));
+class ResizeObserverMock {
+  observe = vi.fn();
+  unobserve = vi.fn();
+  disconnect = vi.fn();
+}
 
 vi.stubGlobal("ResizeObserver", ResizeObserverMock);
 
