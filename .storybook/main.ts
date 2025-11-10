0fdc41d80d8cb415c61774133a0d0816a7979cee
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-11-10 15:47:10 +0100

    Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,11 +10,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
