2742b820628802dde307bb5ad61a0844aa3cce43
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2026-03-04 21:18:35 +0100

    Choose better src

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -45,7 +45,7 @@ export const LG: Story = { args: { size: "lg" } };
 
 export const Contain: Story = {
   args: {
-    src: "https://cdn.icon-icons.com/icons2/2643/PNG/512/avatar_female_woman_person_people_white_tone_icon_159360.png",
+    src: "https://biodesign.stanford.edu/_jcr_content/local-header/_jcr_content/custom-logo.img.full.high.png",
     objectFit: "contain",
   },
 };
